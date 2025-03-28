d55ceb55a186e99776af0befc700bfc2d4d06788
BoomBoomMushroom <dillioncat7@gmail.com>
2025-03-28 19:59:47 -0400

    Starting w/ discovering random programmers
    - Creation and joining of public discovery server
    - Some packets getting started
    - Can enable searching but does nothing yet

---
+++ js/helping-functions.js
@@ -0,0 +1,9 @@
+const UUID_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+function generateRandomString(length, characters){
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
