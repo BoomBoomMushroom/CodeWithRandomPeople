cbff7ead61ab53ebc5868a6c3dbfde3a95a639c9
BoomBoomMushroom <dillioncat7@gmail.com>
2025-03-28 23:50:36 -0400

    Plan Comments for pairing
    Adding some comments and a setinterval to get started with finding a partner and asking them to be your partner
    also added a random number function in the helping-functions (both ends inclusive)

--- js/helping-functions.js
+++ js/helping-functions.js
@@ -7,3 +7,7 @@ function generateRandomString(length, characters){
     }
     return result;
 }
+
+function randomNumberInclusiveInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
